6b26df42fa629df6b3a9cd7fbb7a618d9e267a1d
sebastian <sebastian@prowidesoftware.com>
2016-08-30 20:16:27 -0300

    new example to generate expanded printout for any message content

---
+++ src/com/prowidesoftware/swift/samples/integrator/ExpandedPrintoutExample.java
@@ -0,0 +1,450 @@
+package com.prowidesoftware.swift.samples.integrator;
+
+import java.util.List;
+import java.util.Locale;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.prowidesoftware.swift.BICDirectory;
+import com.prowidesoftware.swift.model.BICRecord;
+import com.prowidesoftware.swift.model.LogicalTerminalAddress;
+import com.prowidesoftware.swift.model.SwiftBlock2Input;
+import com.prowidesoftware.swift.model.SwiftMessage;
+import com.prowidesoftware.swift.model.Tag;
+import com.prowidesoftware.swift.model.field.Field;
+import com.prowidesoftware.swift.model.field.GenericField;
+import com.prowidesoftware.swift.model.mt.MTInfo;
+import com.prowidesoftware.swift.model.mt.MtStandard;
+import com.prowidesoftware.swift.model.mt.mt1xx.MT103;
+import com.prowidesoftware.swift.model.mt.mt2xx.MT202;
+import com.prowidesoftware.swift.model.mt.mt5xx.MT515;
+import com.prowidesoftware.swift.model.mt.mt9xx.MT940;
+import com.prowidesoftware.swift.scheme.Scheme;
+import com.prowidesoftware.swift.scheme.SchemeSequence;
+
+/**
+ * This example shows how to generate text printout of messages generically (for any message type)
+ * using features from Prowide Integrator SDK as the BIC directory to expand BICs and the MTInfo 
+ * helper class to gather sequence labels. 
+ * <br />
+ * It can be used as base code to extend custom pritouts of MT content.
+ * 
+ *<pre> 
+--------------------  Instance Type and Transmission -------------
+Copy sent to SWIFT
+Priority/Delivery: Normal
+------------------------- Message Header -----------------------
+Swift: fin.515
+Sender:		RBDSJPJTBXXX
+		RBC CAPITAL MARKETS (JAPAN) LTD., TOKYO (JAPAN)
+Receiver:	BOJPJPJTXMF1
+		BANK OF JAPAN - (FOR JAPAN'S RESERVES USE ONLY) (JAPAN)
+------------------------- Message Text -------------------------
+-------- A - General Information --------
+20C: Reference
+	Qualifier: Sender's Message Reference
+	Reference: M1999999.1
+23G: Function of the Message
+	Function: NEWM
+22F: Indicator
+	Qualifier: Trade Transaction Type
+	Indicator: TRAD
+-------- A1 - Linkages --------
+20C: Reference
+	Qualifier: Previous Message Reference
+	Reference: M1999999.1
+-------- C - Confirmation Details --------
+98A: Date
+	Qualifier: Trade Date/Time
+	Date: Jul 12, 2016
+98A: Date
+	Qualifier: Settlement Date/Time
+	Date: Jul 15, 2016
+90A: Price
+	Qualifier: Deal Price
+	Percentage Type Code: PRCT
+	Price: 99
+22H: Indicator
+	Qualifier: Buy/Sell Indicator
+	Code: BUYI
+22H: Indicator
+	Qualifier: Payment Indicator
+	Code: APMT
+-------- C1 - Confirmation Parties --------
+95P: Party
+	Qualifier: Seller
+	BIC: BOJPJPJTMF1 - BANK OF JAPAN - (FOR JAPAN'S RESERVES USE ONLY) (JAPAN)
+97A: Account
+	Qualifier: Safekeeping Account
+	Account: MGT10
+-------- C1 - Confirmation Parties --------
+95P: Party
+	Qualifier: Buyer
+	BIC: RCMCUS31XXX - RBC CAPITAL MARKETS, LLC (UNITED STATES OF AMERICA)
+36B: Quantity of Financial Instrument
+	Qualifier: Quantity of Financial Instrument Confirmed
+	Quantity Type Code: FAMT
+	Quantity: 15,000,000
+36B: Quantity of Financial Instrument
+	Qualifier: Quantity of Financial Instrument Ordered
+	Quantity Type Code: FAMT
+	Quantity: 640,843.05
+35B: Identification of the Financial Instrument
+	Qualifier: ISIN
+	ISIN: US99991UUT05
+	Description: FN 999999
+	Description 2: 5
+	Description 3: 2033/01/01
+-------- D - Settlement Details --------
+22F: Indicator
+	Qualifier: Type of Settlement Transaction Indicator
+	Indicator: TRAD
+-------- D1 - Settlement Parties --------
+95P: Party
+	Qualifier: Delivering Agent
+	BIC: CHASUS33XXX - JPMORGAN CHASE BANK, N.A. (UNITED STATES OF AMERICA)
+-------- D1 - Settlement Parties --------
+95P: Party
+	Qualifier: Deliverer's Custodian
+	BIC: CHASJPJTXXX - JPMORGAN CHASE BANK, N.A. - (TOKYO BRANCH) (JAPAN)
+-------- D1 - Settlement Parties --------
+95P: Party
+	Qualifier: Seller
+	BIC: BOJPJPJTMF1 - BANK OF JAPAN - (FOR JAPAN'S RESERVES USE ONLY) (JAPAN)
+-------- D3 - Amounts --------
+19A: Amount
+	Qualifier: Settlement Amount
+	Currency: USD
+	Amount: 234,567
+-------- D3 - Amounts --------
+19A: Amount
+	Qualifier: Trade Amount
+	Currency: USD
+	Amount: 123,456
+-------- D3 - Amounts --------
+19A: Amount
+	Qualifier: Accrued Interest Amount
+	Currency: USD
+	Amount: 1,246.5
+ * </pre> 
+ * 
+ * @author www.prowidesoftware.com
+ * @since 7.8.4
+ */
+public class ExpandedPrintoutExample {
+
+	public static void main(String[] args) {
+
+		MT103 mt103 = MT103.parse("{1:F01BACOARB1A0B20000000000}{2:I103ADRBNL21XXXXN}{4:\n" +
+				":20:REFERENCE\n" + 
+				":23B:CRED\n" + 
+				":32A:130204USD1234567,89\n" + 
+				":50A:/12345678901234567890\n" + 
+				"CFIMHKH1XXX\n" + 
+				":59:/12345678901234567890\n" + 
+				"JOE DOE\n" + 
+				":71A:OUR\n" + 
+				"-}");
+		
+		MT515 mt515 = MT515.parse("{1:F01RBDSJPJTBXXX0000000000}{2:I515BOJPJPJTXMF1N}{4:\n" +
+				":16R:GENL\n" +
+				":20C::SEME//M1999999.1\n" +
+				":23G:NEWM\n" +
+				":22F::TRTR//TRAD\n" +
+				":16R:LINK\n" +
+				":20C::PREV//M1999999.1\n" +
+				":16S:LINK\n" +
+				":16S:GENL\n" +
+				":16R:CONFDET\n" +
+				":98A::TRAD//20160712\n" +
+				":98A::SETT//20160715\n" +
+				":90A::DEAL//PRCT/99,00000000\n" +
+				":22H::BUSE//BUYI\n" +
+				":22H::PAYM//APMT\n" +
+				":16R:CONFPRTY\n" +
+				":95P::SELL//BOJPJPJTMF1\n" +
+				":97A::SAFE//MGT10\n" +
+				":16S:CONFPRTY\n" +
+				":16R:CONFPRTY\n" +
+				":95P::BUYR//RCMCUS31XXX\n" +
+				":16S:CONFPRTY\n" +
+				":36B::CONF//FAMT/15000000,\n" +
+				":36B::ORDR//FAMT/640843,05\n" +
+				":35B:ISIN US99991UUT05\n" +
+				"FN 999999\n" +
+				"5\n" +
+				"2033/01/01\n" +
+				":16S:CONFDET\n" +
+				":16R:SETDET\n" +
+				":22F::SETR//TRAD\n" +
+				":16R:SETPRTY\n" +
+				":95P::DEAG//CHASUS33XXX\n" +
+				":16S:SETPRTY\n" +
+				":16R:SETPRTY\n" +
+				":95P::DECU//CHASJPJTXXX\n" +
+				":16S:SETPRTY\n" +
+				":16R:SETPRTY\n" +
+				":95P::SELL//BOJPJPJTMF1\n" +
+				":16S:SETPRTY\n" +
+				":16R:AMT\n" +
+				":19A::SETT//USD234567,00\n" +
+				":16S:AMT\n" +
+				":16R:AMT\n" +
+				":19A::DEAL//USD123456,00\n" +
+				":16S:AMT\n" +
+				":16R:AMT\n" +
+				":19A::ACRU//USD1246,50\n" +
+				":16S:AMT\n" +
+				":16S:SETDET\n" +
+				"-}");
+		
+		MT940 mt940 = MT940.parse("{1:F01BACOARB1A0B20000000000}{2:I940BSCHGB2LXEQUN}{4:\n" +
+	            ":20:123456\n"+
+				":25:123-304958\n"+
+				":28C:123/1\n"+
+				":60F:C980622USD395212311,71\n"+
+				":61:980623C50000000,NTRFNONREF//8951234\n"+
+				"ORDER BK OF NYC WESTERN CASH RESERVE\n"+
+				":61:980625C5700000,NFEX036960//8954321\n"+
+				":61:980626C200000,NDIVNONREF//8846543\n"+
+				":86:DIVIDEND LORAL CORP\n"+
+				"PREFERRED STOCK 1ST QUARTER 1998\n"+
+				":62F:C980623USD451112311,71\n"+
+				":64:C980623USD445212311,71\n"+
+				":65:C980625USD450912311,71\n"+
+				":65:C980626USD451112311,71\n"+
+				":86:PRIME RATE AS OF TODAY 11 PCT\n"+ 
+	            "-}");
+		
+		MT202 mt202 = MT202.parse("{1:F01BICFOOYYAXXX8628453424}{2:O2021300050901IRVTLULXALTA06556102830509011300N}{4:\n" + 
+		":20:RFSAMPPGN0031091\n" + 
+		":21:RFSAMPPGN0031091\n" + 
+		":13C:/RNCTIME/1356+0000\n" +
+		":13C:/RNCTIME/1410+0000\n" +
+		":32A:050901EUR19265,53\n" + 
+		":52A:IRVTLULXLTA\n" + 
+		":53A:/D/1234A0123456ABC012345\n" + 
+		"BICFOOYY\n" + 
+		":54A:BAPPIT21AP8\n" + 
+		":56A:BMISLBB1025\n" + 
+		":57A:HLFXGB21L17\n" + 
+		":58A:/ES12 1234 6789 1234 1111 1234\n" + 
+		"SEICUS33CAI\n" + 
+		":72:/BNF/00002695 0001 2005083130110\n" + 
+		"-}");
+		
+		//System.out.println(expanded(mt103.getSwiftMessage()));
+		System.out.println(expanded(mt515.getSwiftMessage()));
+		//System.out.println(expanded(mt940.getSwiftMessage()));
+		//System.out.println(expanded(mt202.getSwiftMessage()));
+	}
+	
+	/**
+	 * Takes any message as parameter and returns a formatted printout.
+	 * @param msg SwiftMessage can be loaded from any source using specific MTnnn classes or the generic SwiftParser
+	 * @return formatted print out
+	 */
+	private static String expanded(final SwiftMessage msg) {
+		/*
+		 * create directory instance (only available in Prowide Integrator)
+		 */
+		BICDirectory repo = new BICDirectory();
+		
+		/*
+		 * Create builder for output
+		 */
+		StringBuilder out = new StringBuilder();
+		
+		out.append("--------------------  Instance Type and Transmission -------------").append("\n");
+		
+		if (msg.isIncoming()) {
+			out.append("Copy received from SWIFT").append("\n");
+		} else if (msg.isOutgoing()) {
+			out.append("Copy sent to SWIFT").append("\n");
+		}
+		
+		if (msg.getBlock2().getMessagePriorityType() != null) {
+			out.append("Priority/Delivery: ");
+			out.append(msg.getBlock2().getMessagePriorityType().getLabel());
+			if (msg.isOutgoing()) {
+				SwiftBlock2Input b2i = (SwiftBlock2Input) msg.getBlock2();
+				if (b2i.getDeliveryMonitoringType() != null) {
+					out.append("/").append(b2i.getDeliveryMonitoringType().getLabel());
+				}
+			}
+			out.append("\n");
+		}
+		
+		if (msg.isIncoming()) {
+			out.append("Message Input Reference: ").append(msg.getMIR()).append("\n");;
+		}
+		
+		out.append("------------------------- Message Header -----------------------").append("\n");
+		out.append("Swift: ").append(msg.getMtId().id()).append("\n");
+		
+		/*
+		 * expand sender
+		 */
+		out.append("Sender:\t\t").append(msg.getSender());
+		final String expandedSender = expandLT(repo, msg.getSender());
+		if (expandedSender != null) {
+			out.append("\n\t\t").append(expandedSender);
+		}
+		out.append("\n");
+		
+		/*
+		 * expand receiver
+		 */
+		out.append("Receiver:\t").append(msg.getReceiver());
+		final String expandedReceiver = expandLT(repo, msg.getReceiver());
+		if (expandedReceiver != null) {
+			out.append("\n\t\t").append(expandedReceiver);
+		}
+		out.append("\n");
+		
+		if (msg.getMUR() != null) {
+			out.append("MUR: ").append(msg.getMUR()).append("\n");
+		}
+		
+		out.append("------------------------- Message Text -------------------------").append("\n");
+		
+		/*
+		 * create instances of MTinfo and Scheme (only available in Prowide Integrator)
+		 */
+		Locale locale = Locale.getDefault();
+		MTInfo info = new MTInfo(locale, msg);
+		Scheme scheme = MtStandard.SRU2016.type(msg.getMtId()).scheme();
+		
+		boolean inSequence = false;
+		String currentSequence = null;
+		/*
+		 * iterate message fields detectign sequences start/end
+		 */
+		for (Tag tag : msg.getBlock4().getTags()) {
+			if ("16R".equals(tag.getName())) {
+				/*
+				 * start sequence
+				 */
+				SchemeSequence seq = scheme.getSequenceBy16RQualifier(tag.getValue());
+				currentSequence = seq.getName();
+				inSequence = true;
+				/*
+				 * print sequence name and description
+				 */
+				out.append("-------- ").append(currentSequence).append(" - ").append(info.sequenceName(currentSequence)).append(" --------\n");
+			} else if ("16S".equals(tag.getName())) {
+				/*
+				 * close current sequence
+				 */
+				currentSequence = null;
+			} else if (tag.getNumber() == 15) {
+				currentSequence = tag.getLetterOption();
+				if (inSequence) {
+					/*
+					 * close current sequence
+					 */
+				}
+				inSequence = true;
+				/*
+				 * start sequence
+				 * print sequence name and description
+				 */
+				out.append("-------- ").append(currentSequence).append(" - ").append(info.sequenceName(tag.getLetterOption())).append(" --------\n");
+			} else {
+				/*
+				 * print field components
+				 */
+				String fieldLabel = info.fieldName(tag.getName(), currentSequence);
+				Field f = tag.asField();
+				out.append(f.getName()).append(": ").append(fieldLabel).append("\n");
+				for (int component=1; component<=f.getComponents().size(); component++) {
+					final String value = f.getComponent(component);
+					if (value != null) {
+						final String label = f.getComponentLabel(component);
+						out.append("\t").append(label).append(": ");
+						if (StringUtils.equals("BIC", label)) {
+							/*
+							 * if component is a BIC, expand the institution data
+							 */
+							out.append(value);
+							final String name = expandBIC(repo, value.trim());
+							if (name != null) {
+								out.append(" - ").append(name);
+							}
+						} else {
+							String s = null;
+							if (component == 1 && f instanceof GenericField) {
+								/*
+								 * for generic fields, expand the qualifier
+								 * (this could also be used as more specific field label)
+								 */
+								s = info.qualifierName(f.getName(), value, currentSequence);
+							}
+							if (s == null) {
+								/*
+								 * try to use formatted output from field
+								 */
+								s = f.getValueDisplay(component, locale);
+							}
+							if (s == null) {
+								/*
+								 * default with component value as is
+								 */
+								s = value;
+							}
+							out.append(s);
+						}
+						out.append("\n");
+					}
+				}
+			}
+		}
+		
+		return out.toString();
+	}
+
+	/**
+	 * extracts the BIC from a logical terminal address from message header and 
+	 * calls the expansion method to get the institution name
+	 * @param repo BIC directory catalog
+	 * @param lt a logical terminal address from message header
+	 * @return expanded info or null if bic is not found in the repository
+	 */
+	private static String expandLT(final BICDirectory repo, final String lt) {
+		LogicalTerminalAddress address = new LogicalTerminalAddress(lt);
+		return expandBIC(repo, address.getBic11());
+	}
+	
+	/**
+	 * Searches the parameter bic in the repository, and if found,
+	 * returns a strign with the institution name and, if present,
+	 * branch information and country
+	 * @param repo BIC directory catalog
+	 * @param bic a bic 8 or bic 11 from message header or field BIC components
+	 * @return expanded info or null if bic is not found in the repository
+	 */
+	private static String expandBIC(final BICDirectory repo, final String bic) {
+		List<BICRecord> found = repo.query(bic, true);
+		if (!found.isEmpty()) {
+			if (found.size() > 1) {
+				//log warning, expected one and found n
+			}
+			BICRecord br = found.get(0);
+			final String name = StringUtils.trimToNull(br.getInstitutionName());
+			if (name != null) {
+				StringBuilder result = new StringBuilder();
+				result.append(name);
+				if (br.getBranchInformation() != null) {
+					result.append(" - ").append(br.getBranchInformation());
+				}
+				if (br.getCountryName() != null) {
+					result.append(" (").append(br.getCountryName()).append(")");
+				}
+				return result.toString();
+			}
+		} else {
+			//log warning not found
+		}
+		return null;
+	}
+}
