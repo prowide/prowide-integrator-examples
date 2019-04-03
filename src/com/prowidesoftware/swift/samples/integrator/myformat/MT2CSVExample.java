07eb2ecd267a53e193d2c6f4831c79e9f322fb4d
sebastian <sebastian@prowidesoftware.com>
2019-04-03 11:53:29 -0300

    added second message to example

--- src/com/prowidesoftware/swift/samples/integrator/myformat/MT2CSVExample.java
+++ src/com/prowidesoftware/swift/samples/integrator/myformat/MT2CSVExample.java
@@ -16,6 +16,8 @@ package com.prowidesoftware.swift.samples.integrator.myformat;
 
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.prowidesoftware.swift.myformat.FileFormat;
 import com.prowidesoftware.swift.myformat.MappingRule;
@@ -26,58 +28,20 @@ import com.prowidesoftware.swift.myformat.Transformation.Key;
 import com.prowidesoftware.swift.myformat.csv.CsvFileWriter;
 
 /**
- * This example shows how to convert and MT into a CSV row
+ * This example shows how to convert and MT into a CSV 
  * using API from Prowide Integrator MyFormat module.
  * <br>
  * The mapping rules in this example are defined programmatically.
  * <br>
- * The example takes an MT300 as input, and produces as result an
- * individual CSV row with information gathered from the MT.
- * To create a CSV file with multiples lines, each corresponding to
- * a translated input MT, the translation call should be run on each
- * source message, appending the resulting CSV row to the output File.  
+ * The example takes a couple of MT300 as input, and produces as result an
+ * a CSV  with information gathered from the MTs, one CSV row per source MT.
  * 
- * @author sebastian@prowidesoftware.com
  * @since 7.8
  */
 public class MT2CSVExample {
 	public static void main(String[] args) throws IOException {
-		/*
-		 * source message
-		 */
-		String msg = "{1:F01ABCBUS33AXXX3768156193}{2:O3001139050822XYZBUS33AFXO29569650200508221139N}{3:{108:FC003105ded7970a}}{4:\n" +
-				   ":15A:\n" +
-				   ":20:QCOUCN\n" +
-				   ":21:NEW\n" +
-				   ":22A:CANC\n" +
-				   ":22C:ABCB334209XYZB33\n" +
-				   ":82A:XYZBUS33FXO\n" +
-				   ":87A:ABCBUS33XXX\n" +
-				   ":77D:/VALD/20040509\n" +
-				   "/SETC/USD\n" +
-				   ":15B:\n" +
-				   ":30T:20070422\n" +
-				   ":30V:20070513\n" +
-				   ":36:542,09\n" +
-				   ":32B:CLP3794630000,\n" +
-				   ":53A:XYZBUS33FXO\n" +
-				   ":57D:NET SETTLEMENT\n" +
-				   ":33B:USD7000000,00\n" +
-				   ":53A:ABCBUS33XXX\n" +
-				   ":57D:NET SETTLEMENT\n" +
-				   ":58A:/9301011483\n" +
-				   "ABCBUS33XXX\n" +
-				   ":15C:\n" +
-				   ":24D:BROK\n" +
-				   ":88D:GFI-NY\n" +
-				   ":72:/ACC/GTMS:\n" +
-				   "//L1710833-1-1\n" +
-				   "-}{5:{CHK:97BE21F26A78}}";
 		
-		/*
-		 * programmatic mapping rules
-		 * notice this rules could also be loaded from Excel spreadsheet or database.
-		 */
+		// programmatic mapping rules (this rules could also be loaded from Excel spreadsheet)
 		MappingTable t = new MappingTable(FileFormat.MT, FileFormat.CSV);
 		t.add(new MappingRule("20", "0")); 
 		t.add(new MappingRule("21", "1"));
@@ -85,30 +49,98 @@ public class MT2CSVExample {
 		t.add(new MappingRule("B/32B/2", "3", new Transformation(Key.replace, ",", ".")));
 		t.add(new MappingRule("B/58A/2", "4", new Transformation(Key.prepend, "#")));
 		t.add(new MappingRule("C/72/Line[2]", "5", new Transformation(Key.stripStart, "/")));
+
+		// create the CSV writer.
+		StringWriter out = new StringWriter();
+		CsvFileWriter writer = new CsvFileWriter(out);
+
+		// load the source messages
+		List<String> messages = new ArrayList<>();
+		messages.add(msg);
+		messages.add(msg2);
+
+		// iterate source messages calling translation
+		for (String msg : messages) {
+			
+			// translation call
+			final String line = MyFormatEngine.translate(msg, t);
+		
+			// append row to writer
+			writer.write(line);
+		}
+		
+		// close the writer
+		writer.close();
 		
 		/*
-		 * check if the mapping table is valid
+		 * print output
+		 * QCOUCN111,NEW111,CLP,3794630000.,#9301011483,L1710833-1-1
+		 * QCOUCN222,NEW222,CLP,1234530000.,#848473332,L1710999-2-2
 		 */
-		if (t.validate().isEmpty()) {
-			/*
-			 * translation call
-			 */
-			final String line = MyFormatEngine.translate(msg, t);
-			
-			/*
-			 * print the result:
-			 * QCOUCN,NEW,CLP,3794630000.,#9301011483,L1710833-1-1
-			 */
-			System.out.println(line);
-			
-			/*
-			 * if multiple MT source messages should be processed and put into a single CSV file
-			 * then the CSV file writer can be used.
-			 */
-			StringWriter out = new StringWriter();
-			CsvFileWriter writer = new CsvFileWriter(out);
-			writer.write(line);
-			writer.close();
-		}
+		System.out.println(out);
 	}
+	
+	/*
+	 * source message
+	 */
+	static String msg = "{1:F01ABCBUS33AXXX3768156193}{2:O3001139050822XYZBUS33AFXO29569650200508221139N}{3:{108:FC003105ded7970a}}{4:\n" +
+			   ":15A:\n" +
+			   ":20:QCOUCN111\n" +
+			   ":21:NEW111\n" +
+			   ":22A:CANC\n" +
+			   ":22C:ABCB334209XYZB33\n" +
+			   ":82A:XYZBUS33FXO\n" +
+			   ":87A:ABCBUS33XXX\n" +
+			   ":77D:/VALD/20040509\n" +
+			   "/SETC/USD\n" +
+			   ":15B:\n" +
+			   ":30T:20070422\n" +
+			   ":30V:20070513\n" +
+			   ":36:542,09\n" +
+			   ":32B:CLP3794630000,\n" +
+			   ":53A:XYZBUS33FXO\n" +
+			   ":57D:NET SETTLEMENT\n" +
+			   ":33B:USD7000000,00\n" +
+			   ":53A:ABCBUS33XXX\n" +
+			   ":57D:NET SETTLEMENT\n" +
+			   ":58A:/9301011483\n" +
+			   "ABCBUS33XXX\n" +
+			   ":15C:\n" +
+			   ":24D:BROK\n" +
+			   ":88D:GFI-NY\n" +
+			   ":72:/ACC/GTMS:\n" +
+			   "//L1710833-1-1\n" +
+			   "-}";
+	
+	/*
+	 * source message
+	 */
+	static String msg2 = "{1:F01ABCBUS33AXXX3768156193}{2:O3001139050822XYZBUS33AFXO29569650200508221139N}{3:{108:FC003105ded7970a}}{4:\n" +
+			   ":15A:\n" +
+			   ":20:QCOUCN222\n" +
+			   ":21:NEW222\n" +
+			   ":22A:CANC\n" +
+			   ":22C:ABCB334209XYZB33\n" +
+			   ":82A:XYZBUS33FXO\n" +
+			   ":87A:ABCBUS33XXX\n" +
+			   ":77D:/VALD/20040509\n" +
+			   "/SETC/USD\n" +
+			   ":15B:\n" +
+			   ":30T:20070422\n" +
+			   ":30V:20070513\n" +
+			   ":36:542,09\n" +
+			   ":32B:CLP1234530000,\n" +
+			   ":53A:XYZBUS33FXO\n" +
+			   ":57D:NET SETTLEMENT\n" +
+			   ":33B:USD7000000,00\n" +
+			   ":53A:ABCBUS33XXX\n" +
+			   ":57D:NET SETTLEMENT\n" +
+			   ":58A:/848473332\n" +
+			   "ABCBUS33XXX\n" +
+			   ":15C:\n" +
+			   ":24D:BROK\n" +
+			   ":88D:GFI-NY\n" +
+			   ":72:/ACC/GTMS:\n" +
+			   "//L1710999-2-2\n" +
+			   "-}";
 }
