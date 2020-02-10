dd2655bb21188c1550953dd9a84140cf87d01640
zubri <sebastian@prowidesoftware.com>
2020-02-10 20:41:20 -0300

    completed example

--- src/main/java/com/prowidesoftware/swift/samples/integrator/myformat/Mx2CsvExample.java
+++ src/main/java/com/prowidesoftware/swift/samples/integrator/myformat/Mx2CsvExample.java
@@ -15,9 +15,7 @@
 package com.prowidesoftware.swift.samples.integrator.myformat;
 
 import com.prowidesoftware.swift.model.mx.MxPain00100103;
-import com.prowidesoftware.swift.myformat.FileFormat;
-import com.prowidesoftware.swift.myformat.MappingRule;
-import com.prowidesoftware.swift.myformat.MappingTable;
+import com.prowidesoftware.swift.myformat.*;
 
 import java.io.IOException;
 
@@ -32,61 +30,54 @@ public class Mx2CsvExample {
         // source MX is pain.001.001.03 including two payment info,
 
         String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-                " <Doc:Document xmlns:Doc=\"urn:iso:std:iso:20022:tech:xsd:pain.001.001.03\">\n" +
-                "  <Doc:CstmrCdtTrfInitn>\n" +
-                "   <Doc:PmtInf>\n" +
-                "    <Doc:DbtrAcct>\n" +
-                "     <Doc:Id>\n" +
-                "      <Doc:IBAN>DE12345678901234567890</Doc:IBAN>\n" +
-                "     </Doc:Id>\n" +
-                "    </Doc:DbtrAcct>\n" +
-                "   \n" +
-                "    <Doc:CdtTrfTxInf>\n" +
-                "     <Doc:Amt>\n" +
-                "      <Doc:InstdAmt Ccy=\"USD\" >1234.56</Doc:InstdAmt>\n" +
-                "     </Doc:Amt>\n" +
-                "    \n" +
-                "     <Doc:CdtrAcct>\n" +
-                "      <Doc:Id>\n" +
-                "       <Doc:IBAN>US12345678901234567890</Doc:IBAN>\n" +
-                "      </Doc:Id>\n" +
-                "     </Doc:CdtrAcct>\n" +
-                "    </Doc:CdtTrfTxInf>\n" +
-                "   </Doc:PmtInf>\n" +
-                "  \n" +
-                "   <Doc:PmtInf>\n" +
-                "    <Doc:DbtrAcct>\n" +
-                "     <Doc:Id>\n" +
-                "      <Doc:IBAN>ES12345678901234567890</Doc:IBAN>\n" +
-                "     </Doc:Id>\n" +
-                "    </Doc:DbtrAcct>\n" +
-                "   \n" +
-                "    <Doc:CdtTrfTxInf>\n" +
-                "     <Doc:Amt>\n" +
-                "      <Doc:InstdAmt Ccy=\"USD\" >2345.67</Doc:InstdAmt>\n" +
-                "     </Doc:Amt>\n" +
-                "    \n" +
-                "     <Doc:CdtrAcct>\n" +
-                "      <Doc:Id>\n" +
-                "       <Doc:IBAN>CA12345678901234567890</Doc:IBAN>\n" +
-                "      </Doc:Id>\n" +
-                "     </Doc:CdtrAcct>\n" +
-                "    </Doc:CdtTrfTxInf>\n" +
-                "   \n" +
-                "    <Doc:CdtTrfTxInf>\n" +
-                "     <Doc:Amt>\n" +
-                "      <Doc:InstdAmt Ccy=\"EUR\" >6789.10</Doc:InstdAmt>\n" +
-                "     </Doc:Amt>\n" +
-                "    \n" +
-                "     <Doc:CdtrAcct>\n" +
-                "      <Doc:Id>\n" +
-                "       <Doc:IBAN>JP12345678901234567890</Doc:IBAN>\n" +
-                "      </Doc:Id>\n" +
-                "     </Doc:CdtrAcct>\n" +
-                "    </Doc:CdtTrfTxInf>\n" +
-                "   </Doc:PmtInf>\n" +
-                "  </Doc:CstmrCdtTrfInitn>\n" +
-                " </Doc:Document>";
+        "<Doc:Document xmlns:Doc=\"urn:iso:std:iso:20022:tech:xsd:pain.001.001.03\">\n" +
+        "   <Doc:CstmrCdtTrfInitn>\n" +
+        "       <Doc:PmtInf>\n" +
+        "           <Doc:DbtrAcct>\n" +
+        "               <Doc:Id>\n" +
+        "                   <Doc:IBAN>DE12345678901234567890</Doc:IBAN>\n" +
+        "               </Doc:Id>\n" +
+        "           </Doc:DbtrAcct>\n" +
+        "           <Doc:CdtTrfTxInf>\n" +
+        "               <Doc:Amt>\n" +
+        "                   <Doc:InstdAmt Ccy=\"USD\" >1234.56</Doc:InstdAmt>\n" +
+        "               </Doc:Amt>\n" +
+        "               <Doc:CdtrAcct>\n" +
+        "                   <Doc:Id>\n" +
+        "                       <Doc:IBAN>US12345678901234567890</Doc:IBAN>\n" +
+        "                   </Doc:Id>\n" +
+        "               </Doc:CdtrAcct>\n" +
+        "           </Doc:CdtTrfTxInf>\n" +
+        "       </Doc:PmtInf>\n" +
+        "       <Doc:PmtInf>\n" +
+        "           <Doc:DbtrAcct>\n" +
+        "               <Doc:Id>\n" +
+        "                   <Doc:IBAN>ES12345678901234567890</Doc:IBAN>\n" +
+        "               </Doc:Id>\n" +
+        "           </Doc:DbtrAcct>\n" +
+        "           <Doc:CdtTrfTxInf>\n" +
+        "               <Doc:Amt>\n" +
+        "                   <Doc:InstdAmt Ccy=\"USD\" >2345.67</Doc:InstdAmt>\n" +
+        "               </Doc:Amt>\n" +
+        "               <Doc:CdtrAcct>\n" +
+        "                   <Doc:Id>\n" +
+        "                       <Doc:IBAN>CA12345678901234567890</Doc:IBAN>\n" +
+        "                   </Doc:Id>\n" +
+        "               </Doc:CdtrAcct>\n" +
+        "           </Doc:CdtTrfTxInf>\n" +
+        "           <Doc:CdtTrfTxInf>\n" +
+        "               <Doc:Amt>\n" +
+        "                   <Doc:InstdAmt Ccy=\"EUR\" >6789.10</Doc:InstdAmt>\n" +
+        "               </Doc:Amt>\n" +
+        "               <Doc:CdtrAcct>\n" +
+        "                   <Doc:Id>\n" +
+        "                       <Doc:IBAN>JP12345678901234567890</Doc:IBAN>\n" +
+        "                   </Doc:Id>\n" +
+        "               </Doc:CdtrAcct>\n" +
+        "           </Doc:CdtTrfTxInf>\n" +
+        "       </Doc:PmtInf>\n" +
+        "   </Doc:CstmrCdtTrfInitn>\n" +
+        "</Doc:Document>";
 
         MxPain00100103 mx = MxPain00100103.parse(xml);
 
@@ -94,18 +85,35 @@ public class Mx2CsvExample {
 
         MappingTable t = new MappingTable(FileFormat.MX, FileFormat.CSV);
 
+        // Initialize target CSV with one row per ignore repetition CdtTrfTxInf in source MX.
+        // Since PmtInf is also repetitive, we could have one row per PmtInf as well, it just depend on how we want to
+        // serialize the MX nested structure into the CSV rows.
+        t.add(new MappingRule("FOREACH(/Document/CstmrCdtTrfInitn/PmtInf/CdtTrfTxInf, \",,,\")",
+                "addRow", WriteMode.SETUP));
+
         // ignore repetition of PmtInf
-        t.add(new MappingRule("/Document/CstmrCdtTrfInitn/PmtInf/CdtTrfTxInf[m]/CdtrAcct/Id/IBAN", "1"));
-        t.add(new MappingRule("/Document/CstmrCdtTrfInitn/PmtInf/CdtTrfTxInf[m]/Amt/InstdAmt", "2"));
-        t.add(new MappingRule("/Document/CstmrCdtTrfInitn/PmtInf/CdtTrfTxInf[m]/Amt/InstdAmt/@Ccy", "3"));
+        t.add(new MappingRule("foreach(/Document/CstmrCdtTrfInitn/PmtInf/CdtTrfTxInf[{n}]/CdtrAcct/Id/IBAN)", "1"));
+        t.add(new MappingRule("foreach(/Document/CstmrCdtTrfInitn/PmtInf/CdtTrfTxInf[{m}]/Amt/InstdAmt)", "2"));
+        t.add(new MappingRule("foreach(/Document/CstmrCdtTrfInitn/PmtInf/CdtTrfTxInf[{m}]/Amt/InstdAmt/@Ccy)", "3"));
         // then for each CdtTrfTxInf use relative path to get the parent debtor account
-        t.add(new MappingRule("/Document/CstmrCdtTrfInitn/PmtInf/CdtTrfTxInf[m]/../DbtrAcct/Id/IBAN", "0"));
+        t.add(new MappingRule("foreach(/Document/CstmrCdtTrfInitn/PmtInf/CdtTrfTxInf[{m}]/, ../DbtrAcct/Id/IBAN)", "0"));
 
         // group by PmtInf DbtrAcct
-        t.add(new MappingRule("/Document/CstmrCdtTrfInitn/PmtInf[n]/DbtrAcct/Id/IBAN", "0"));
-        t.add(new MappingRule("/Document/CstmrCdtTrfInitn/PmtInf[n:/DbtrAcct/Id/IBAN]/CdtTrfTxInf[m]/CdtrAcct/Id/IBAN", "1"));
-        t.add(new MappingRule("/Document/CstmrCdtTrfInitn/PmtInf[n:/DbtrAcct/Id/IBAN]/CdtTrfTxInf[m]/Amt/InstdAmt", "2"));
-        t.add(new MappingRule("/Document/CstmrCdtTrfInitn/PmtInf[n:/DbtrAcct/Id/IBAN]/CdtTrfTxInf[m]/Amt/InstdAmt/@Ccy", "3"));
+        t.add(new MappingRule("foreach(/Document/CstmrCdtTrfInitn/PmtInf[n]/DbtrAcct/Id/IBAN)", "0"));
+        t.add(new MappingRule("foreach(/Document/CstmrCdtTrfInitn/PmtInf[n:/DbtrAcct/Id/IBAN]/CdtTrfTxInf[m]/CdtrAcct/Id/IBAN)", "1"));
+        t.add(new MappingRule("foreach(/Document/CstmrCdtTrfInitn/PmtInf[n:/DbtrAcct/Id/IBAN]/CdtTrfTxInf[m]/Amt/InstdAmt)", "2"));
+        t.add(new MappingRule("foreach(/Document/CstmrCdtTrfInitn/PmtInf[n:/DbtrAcct/Id/IBAN]/CdtTrfTxInf[m]/Amt/InstdAmt/@Ccy)", "3"));
+
+        // call translation
+        final String csv = MyFormatEngine.translate(mx.message(), t);
+
+        // print the created output (this could be written into a file)
+        System.out.println(csv);
+
+        // expected output is:
+        // DE12345678901234567890,US12345678901234567890,1234.56,USD
+        // ES12345678901234567890,CA12345678901234567890,2345.67,USD
+        // ES12345678901234567890,JP12345678901234567890,6789.10,EUR
     }
 
 }
